c1367f97b09106d1a860279cb4d61c053ae74443
HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2024-02-06 11:17:59 +0900

    Add files via upload

---
+++ DailyQuiz/0205/Grocery.java
@@ -0,0 +1,20 @@
+package chapter08;
+
+public class Grocery extends Product {
+    public Grocery(String name, int price, double weight) {
+        super(name, price, weight);
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public int getDiscountAmount() {
+        return 2000;
+    }
+}
