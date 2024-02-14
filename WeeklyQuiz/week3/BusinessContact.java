9ede3706fda05cebce17de8209b7f66a27cce37b
HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2024-02-14 10:25:23 +0900

    Update BusinessContact.java

--- WeeklyQuiz/week3/BusinessContact.java
+++ WeeklyQuiz/week3/BusinessContact.java
@@ -2,6 +2,7 @@ package chapter10;
 
 public class BusinessContact extends Contact{
     private String company;
+
     BusinessContact(String name, String phoneNumber, String company) {
         this.setName(name);
         this.setPhoneNumber(phoneNumber);
@@ -13,5 +14,10 @@ public class BusinessContact extends Contact{
     }
     public void setCompany(String company) {
         this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, 회사명: %s", super.toString(), company);
     }
 }
